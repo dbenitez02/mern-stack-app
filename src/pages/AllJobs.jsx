f15ba992d375f474e575ee806480b5c1f7733d64
dbenitez02 <dbenitez101@gmail.com>
2023-10-16 01:18:28 -0700

    Setting up

---
+++ src/pages/AllJobs.jsx
@@ -0,0 +1,9 @@
+const AllJobs = () => {
+    return(
+        <div>
+            <h1>AllJobs page</h1>
+        </div>
+    );
+}
+
+export default AllJobs;
---
+++ src/pages/DeleteJob.jsx
@@ -0,0 +1,9 @@
+const DeleteJob = () => {
+    return(
+        <div>
+            <h1>DeleteJob page</h1>
+        </div>
+    );
+}
+
+export default DeleteJob;
