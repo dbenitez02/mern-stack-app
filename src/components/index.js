2c5fe4026ffe344d44d5aad02750350d02297536
dbenitez02 <dbenitez101@gmail.com>
2023-12-01 23:06:33 -0800

    added content to DashboardLayout and added component FormRowSelect

--- src/components/index.js
+++ src/components/index.js
@@ -5,3 +5,4 @@ export { default as BigSidebar } from "./BigSidebar";
 export { default as Navbar } from "./Navbar";
 export { default as LogoutContainer } from "./LogoutContainer";
 export { default as ThemeToggle } from "./ThemeToggle";
+export { default as FormRowSelect } from "./FormRowSelect";
--- src/pages/AddJob.jsx
+++ src/pages/AddJob.jsx
@@ -1,8 +1,34 @@
+import { FormRow, FormRowSelect } from '../components';
+import Wrapper from '../wrappers/DashboardFormPage';
+import { useOutletContext } from 'react-router-dom';
+import { JOB_STATUS, JOB_TYPE } from '../../../utils/constants';
+import { Form, useNavigation, redirect } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import customFetch from '../utils/customFetch';
+
 const AddJob = () => {
+    const {user} = useOutletContext();
+    const navigation = useNavigation();
+    const isSubmitting = navigation.state === 'submitting';
+
     return(
-        <div>
-            <h1>AddJob Page</h1>
-        </div>
+        <Wrapper>
+            <Form method='post' className='form'>
+                <h4 className='form-title'>Add Job</h4>
+                <div className='form-center'>
+                    <FormRow type='text' name='position'/>
+                    <FormRow type='text' name='company'/>
+                    <FormRow type='text' labelText='Job Location' name='jobLocation' defualtValue={user.location}/>
+                    <FormRowSelect labelText='Job Status' name='jobStatus' defualtValue={JOB_STATUS.PENDING} list={Object.values(JOB_STATUS)}/>
+                    <FormRowSelect labelText='Job Type' name='jobType' defualtValue={JOB_TYPE.FULL_TIME} list={Object.values(JOB_TYPE)}/>
+
+                    <button type='submit' className='btn btn-block form-btn' disabled={isSubmitting}>
+                        {isSubmitting ? 'submitting' : 'submit'}
+                    </button>
+
+                </div>
+            </Form>
+        </Wrapper>
     );
 }
 
